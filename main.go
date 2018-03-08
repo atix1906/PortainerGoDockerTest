65f76c7e8c629fe43b2a1e605767c37c79584fbc
Thomas H <useradmin@mobile-119.win.zlw-ima.rwth-aachen.de>
2018-03-08 13:07:25 +0100

    example

--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"crypto/subtle"
-	"fmt"
 	"io"
-	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
 )
-
-type data struct {
-	Username string
-	Password string
-}
 
 func main() {
 	ctx := context.Background()
@@ -57,32 +47,4 @@ func main() {
 	}
 
 	io.Copy(os.Stdout, out)
-
-	v := url.Values{}
-	v.Set("Username", "user")
-	v.Add("Password", "pw")
-	auth, err := http.Post("url", "nosniff", strings.NewReader(v.Encode()))
-	if err != nil {
-		panic(err)
-	}
-	auth1, err1 := http.Get("url")
-
-	if err1 != nil {
-		panic(err1)
-	}
-	fmt.Println(auth)
-	fmt.Println(auth1)
-}
-
-func BasicAuth(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		user, pass, ok := r.BasicAuth()
-		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
-			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
-			w.WriteHeader(401)
-			w.Write([]byte(http.StatusText(401)))
-			return
-		}
-		handler(w, r)
-	}
 }
