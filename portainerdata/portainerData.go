a566baf7e9bdbb01dfe7a9aaa5277f3ced50bc32
atix1906 <th.he.1906@gmail.com>
2018-03-09 16:16:31 +0100

    integrated portainer data

---
+++ portainerdata/portainerData.go
@@ -0,0 +1,13 @@
+package portainerdata
+
+// Username for portainer login
+var Username = `user`
+
+// Password for portainer login
+var Password = `password`
+
+// URL for portainer api
+var URL = `url`
+
+// EndpointID for testing get stack
+var EndpointID = `id`
